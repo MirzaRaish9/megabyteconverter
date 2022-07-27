aea5613de3be711bccbf308c374ec75709690491
Raish Mirza <f.mirza.raish99@gmail.com>
2022-07-27 20:24:23 +0530

    megabyte main.jva

---
+++ main.java
@@ -0,0 +1,11 @@
+public class MainJva {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       
+		
+		MegaBytesConverter.printMegaBytAndKilloBytes(2500);
+		MegaBytesConverter.printMegaBytAndKilloBytes(-100);
+	}
+
+}
